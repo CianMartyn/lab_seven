c734604e34619fdb0853e3c699f70275e00b1832
Cian Martyn <cianmartyn1@gmail.com>
2023-11-17 12:28:03 +0000

    Lab 7

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,12 +7,12 @@ const cors = require('cors');
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 // npm i body-parser
@@ -20,66 +20,60 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Post method to /api/books
-app.post('/api/books', (req, res)=>{
-    console.log(req.body);
-    res.send("Data Received");
+// getting-started.js
+const mongoose = require('mongoose');
 
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.lf4uq0d.mongodb.net/MYDB?retryWrites=true&w=majority');
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    cover: String,
+    author: String,
+})
+
+const bookModel = mongoose.model('CiansBooks', bookSchema);
+
+// Post method to /api/books
+app.post('/api/books', (req, res) => {
+    console.log(req.body);
+    bookModel.create({
+        title: req.body.title,
+        cover: req.body.cover,
+        author: req.body.author
+    })
+        .then(() => { res.send("Book Created!") })
+        .catch(() => { res.send("Book NOT Created!") })
 })
 
 // Hello world on opening page
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+// Book data
+app.get('/api/books', async(req, res) => {
 
-app.get('/api/books', (req, res)=>{
+    let books = await bookModel.find({})
+    res.json(books)
 
-    const books = [{
-        "title": "Learn Git in a Month of Lunches",
-        "isbn": "1617292419",
-        "pageCount": 0,
-        "thumbnailUrl":
-        "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg", "status": "MEAP",
-        "authors": ["Rick Umali"],
-        "categories": []
-        },
-        {
-        "title": "MongoDB in Action, Second Edition",
-        "isbn": "1617291609",
-        "pageCount": 0,
-        "thumbnailUrl":
-        "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-        "status": "MEAP",
-        "authors": [
-        "Kyle Banker",
-        "Peter Bakkum",
-        "Tim Hawkins",
-        "Shaun Verch",
-        "Douglas Garrett"
-        ],
-        "categories": []
-        },
-        {
-        "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-        "isbn": "1617292036",
-        "pageCount": 0,
-        "thumbnailUrl":
-        "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-        "status": "MEAP",
-        "authors": ["Simon Holmes"],
-        "categories": []
-        }];
+})
 
-    res.json({
-        myBooks:books,
-        "Message":"Some information",
-        "Status":"Happy"
+// Read book id
+app.get('/api/book/:id', async(req, res)=>{
+    console.log(req.params.id);
 
-    })
+    let book = await bookModel.findById(req.params.id);
+    res.send(book)
+
 })
 
 // Consolelog
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
 
